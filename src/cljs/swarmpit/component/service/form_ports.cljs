7b66709b9063976651418f4c29578587b71a8e7b
Pavol Noha <pavol.noha@gmail.com>
2017-02-17 15:54:57 +0100

    service ports form

---
+++ src/cljs/swarmpit/component/service/form_ports.cljs
@@ -0,0 +1,97 @@
+(ns swarmpit.component.service.form-ports
+  (:require [swarmpit.material :as material :refer [svg]]
+            [swarmpit.utils :refer [remove-el]]
+            [rum.core :as rum]))
+
+(enable-console-print!)
+
+(defonce state (atom []))
+
+(def form-headers ["Container port" "Protocol" "Published" "Host port"])
+
+(defn- add-item
+  "Create new form item"
+  []
+  (swap! state
+         (fn [p] (conj p {:containerPort ""
+                          :protocol      "tcp"
+                          :published     false
+                          :hostPort      ""}))))
+
+(defn- remove-item
+  "Remove form item"
+  [index]
+  (swap! state
+         (fn [p] (remove-el p index))))
+
+(defn- update-item
+  "Update form item configuration"
+  [index k v]
+  (swap! state
+         (fn [p] (assoc-in p [index k] v))))
+
+(defn- form-container [value index]
+  (material/table-row-column
+    nil
+    (material/text-field
+      #js {:id       "containerPort"
+           :value    value
+           :onChange (fn [e v] (update-item index :containerPort v))})))
+
+(defn- form-protocol [value index]
+  (material/table-row-column
+    nil
+    (material/select-field
+      #js {:value      value
+           :onChange   (fn [e i v] (update-item index :protocol v))
+           :style      #js {:display "inherit"}
+           :labelStyle #js {:lineHeight "45px"
+                            :top        2}}
+      (material/menu-item
+        #js {:key         "tcp"
+             :value       "tcp"
+             :primaryText "TCP"})
+      (material/menu-item
+        #js {:key         "udp"
+             :value       "udp"
+             :primaryText "UDP"}))))
+
+(defn- form-published [value index]
+  (material/table-row-column
+    nil
+    (material/checkbox
+      #js {:checked value
+           :onCheck (fn [e v] (update-item index :published v))})))
+
+(defn- form-host [value index]
+  (material/table-row-column
+    #js {:key index}
+    (material/text-field
+      #js {:id       "hostPort"
+           :value    value
+           :onChange (fn [e v] (update-item index :hostPort v))})))
+
+(rum/defc form < rum/reactive []
+  (let [ports (rum/react state)]
+    [:form
+     [:fieldset
+      [:legend "Ports"]
+      (material/theme
+        (material/table
+          #js {:selectable false}
+          (material/table-header-form form-headers #(add-item))
+          (material/table-body
+            #js {:displayRowCheckbox false}
+            (for [index (range (count ports))]
+              (let [port (nth ports index)
+                    {:keys [containerPort
+                            protocol
+                            published
+                            hostPort]} port]
+                (material/table-row-form
+                  index
+                  [(form-container containerPort index)
+                   (form-protocol protocol index)
+                   (form-published published index)
+                   (form-host hostPort index)]
+                  (fn [] (remove-item index))))))))]]))
