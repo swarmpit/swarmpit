5e54432e1cced8d8169d955728b6208290a535f0
Pavol Noha <pavol.noha@gmail.com>
2017-02-21 14:25:48 +0100

    dev repl new config based on chestnut template

---
+++ dev/user.clj
@@ -0,0 +1,18 @@
+(ns user
+  (:require [swarmpit.server]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [figwheel-sidecar.repl-api :as figwheel]))
+
+;; Let Clojure warn you when it needs to reflect on types, or when it does math
+;; on unboxed numbers. In both cases you should add type annotations to prevent
+;; degraded performance.
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* :warn-on-boxed)
+
+(def http-handler
+  (wrap-reload #'swarmpit.server/handler))
+
+(defn run []
+  (figwheel/start-figwheel!))
+
+(def browser-repl figwheel/cljs-repl)
