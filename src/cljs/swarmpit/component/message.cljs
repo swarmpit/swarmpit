328ac08bb6c49acad138cc45bf7c38f0d4b741db
Pavol Noha <pavol.noha@gmail.com>
2017-04-12 15:51:40 +0200

    message comp reactive -> static

--- src/cljs/swarmpit/component/message.cljs
+++ src/cljs/swarmpit/component/message.cljs
@@ -4,18 +4,13 @@
 
 (enable-console-print!)
 
-(defonce state (atom {:duration 4000
-                      :message  ""
-                      :open     false}))
-
-(rum/defc info < rum/reactive []
-  (let [{:keys [duration message open]} (rum/react state)]
-    (material/theme
-      (material/snackbar
-        #js {:open             open
-             :message          message
-             :autoHideDuration duration}))))
+(rum/defc message < rum/static [text]
+  (material/theme
+    (material/snackbar
+      #js {:open             true
+           :message          text
+           :autoHideDuration 4000})))
 
 (defn mount!
-  []
-  (rum/mount (info) (.getElementById js/document "info")))
+  [text]
+  (rum/mount (message text) (.getElementById js/document "message")))
