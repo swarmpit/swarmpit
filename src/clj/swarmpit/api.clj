6c40131d0de056c303c689958ea554e977d7810d
Pavol Noha <pavol.noha@gmail.com>
2017-04-12 15:51:17 +0200

    service remove api

--- src/clj/swarmpit/api.clj
+++ src/clj/swarmpit/api.clj
@@ -1,9 +1,11 @@
 (ns swarmpit.api
   (:require [swarmpit.docker.client :as dc]
-            [swarmpit.registry.client :as rc]
-            ;[swarmpit.domain :refer [Service Port]]
-            ))
+            [swarmpit.registry.client :as rc]))
 
 (defn services
   ([] (dc/get "/services"))
   ([id] (dc/get (str "/services/" id))))
+
+(defn remove-service
+  [id]
+  (dc/delete (str "/services/" id)))
--- src/clj/swarmpit/handler.clj
+++ src/clj/swarmpit/handler.clj
@@ -14,7 +14,13 @@
   [_]
   {:status 201 :body "Index"})
 
+(defn service-delete
+  [{:keys [route-params]}]
+  (api/remove-service (:id route-params))
+  {:status 200})
+
 (def handler
-  (make-handler ["/" {"services" {:get  {""        services
-                                         ["/" :id] service}
-                                  :post service-create}}]))
+  (make-handler ["/" {"services" {:get    {""        services
+                                           ["/" :id] service}
+                                  :post   service-create
+                                  :delete {["/" :id] service-delete}}}]))
