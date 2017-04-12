e56fbddf159d891ab98fda8c864be44348c25834
Pavol Noha <pavol.noha@gmail.com>
2017-04-12 15:50:40 +0200

    cleanup startup mounting

--- src/cljs/swarmpit/ui.cljs
+++ src/cljs/swarmpit/ui.cljs
@@ -1,10 +1,9 @@
 (ns swarmpit.ui
   (:require [swarmpit.controller :as controller]
-            [swarmpit.component.layout :as layout]
-            [swarmpit.component.info :as info]))
+            [swarmpit.component.layout :as layout]))
 
 (js/console.log "Swarmpit ...")
 
 (layout/mount!)
-(info/mount!)
+
 (controller/start)
