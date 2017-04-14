a4ae29dec62476cc592880d2ffb480bab41d510e
Pavol Noha <pavol.noha@gmail.com>
2017-04-14 13:30:20 +0200

    docker client cleanup

--- src/clj/swarmpit/docker/client.clj
+++ src/clj/swarmpit/docker/client.clj
@@ -2,7 +2,7 @@
   (:refer-clojure :exclude [get])
   (:require [clojure.java.shell :as shell]
             [clojure.string :as string]
-            [cheshire.core :refer [parse-string]]))
+            [cheshire.core :refer [parse-string generate-string]]))
 
 (def ^:private api-version "v1.24")
 (def ^:private base-cmd ["curl" "--unix-socket" "/var/run/docker.sock" "-w" "%{http_code}"])
@@ -26,7 +26,7 @@
   [json]
   (if (nil? json)
     []
-    ["-d" json]))
+    ["-d" (generate-string json {:pretty true})]))
 
 (defn- command
   "Build docker command"
@@ -39,33 +39,35 @@
         (into ppayload)
         (into pcommand))))
 
-(defn- parse-payload
+(defn- parse-body
+  "Parse docker engine response body"
   [response]
   (if (= (count response) 1)
-    nil
+    ""
     (parse-string (first response) true)))
 
 (defn- parse-http-code
+  "Parse docker engine response http code"
   [response]
-  (if (= (count response) 1)
-    (Integer. (first response))
-    (Integer. (second response))))
+  (Integer. (if (= (count response) 1)
+              (first response)
+              (second response))))
 
 (defn- execute
   "Execute docker command and parse result"
   [method uri headers payload]
   (let [cmd (command method uri headers payload)
-        result (apply shell/sh cmd)]
-    (if (= 0 (:exit result))
-      (let [response (string/split (:out result) #"\n")
-            payload (parse-payload response)
-            http-code (parse-http-code response)]
-        (if (> 400 http-code)
-          payload
+        cmd-result (apply shell/sh cmd)]
+    (if (= 0 (:exit cmd-result))
+      (let [response (string/split (:out cmd-result) #"\n")
+            response-body (parse-body response)
+            response-code (parse-http-code response)]
+        (if (> 400 response-code)
+          response-body
           (throw (ex-info "Docker engine error!"
-                          (assoc payload :code http-code)))))
+                          (assoc response-body :code response-code)))))
       (throw (ex-info "Docker client failure!"
-                      (parse-string (:err result) true))))))
+                      (parse-string (:err cmd-result) true))))))
 
 (defn get
   ([uri] (execute "GET" uri nil nil))
