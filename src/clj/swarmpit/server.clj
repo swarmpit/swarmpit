70fbc040e447e98b150f3c2523af6c8eda8e93b4
Pavol Noha <pavol.noha@gmail.com>
2017-05-09 20:34:39 +0200

    added auth checkin

--- src/clj/swarmpit/server.clj
+++ src/clj/swarmpit/server.clj
@@ -1,9 +1,23 @@
 (ns swarmpit.server
   (:import (clojure.lang ExceptionInfo))
   (:use [org.httpkit.server :only [run-server]])
-  (:require [swarmpit.handler :refer [handler]]
-            [ring.middleware.json :as ring-json]
-            [cheshire.core :refer [parse-string]]))
+  (:require [ring.middleware.json :as ring-json]
+            [cheshire.core :refer [parse-string]]
+            [swarmpit.handler :refer [handler]]
+            [swarmpit.utils :refer [in?]]))
+
+(def unsecure ["/login"])
+
+(defn- secure?
+  [request]
+  (let [uri (:uri request)]
+    (not (in? unsecure uri))))
+
+(defn- json-response
+  [status response]
+  {:status  status
+   :headers {"Content-Type" "application/json"}
+   :body    {:error response}})
 
 (defn wrap-client-exception
   [handler]
@@ -12,9 +26,8 @@
       (handler request)
       (catch ExceptionInfo e
         (let [response (ex-data e)]
-          {:status  (:code response)
-           :headers {"Content-Type" "application/json"}
-           :body    {:error (:message response)}})))))
+          (json-response (:code response)
+                         (:message response)))))))
 
 (defn wrap-fallback-exception
   [handler]
@@ -24,8 +37,22 @@
       (catch Exception e
         {:status 500 :body e}))))
 
+(defn wrap-authentication
+  [handler]
+  (fn [request]
+    (if (secure? request)
+      (let [headers (:headers request)
+            token (get headers "authorization")]
+        (if (some? token)
+          (if true
+            (handler request)
+            (json-response 401 "Invalid token"))
+          (json-response 400 "Missing token")))
+      (handler request))))
+
 (def app
   (-> handler
+      wrap-authentication
       wrap-client-exception
       ring-json/wrap-json-response
       ring-json/wrap-json-params
