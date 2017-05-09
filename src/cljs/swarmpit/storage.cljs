b2d79553cd559c3b79be5cdbc0160a3af951942e
Pavol Noha <pavol.noha@gmail.com>
2017-05-09 20:34:21 +0200

    add basic storage utils

---
+++ src/cljs/swarmpit/storage.cljs
@@ -0,0 +1,16 @@
+(ns swarmpit.storage)
+
+(defn set-item!
+  "Set `key' in browser's localStorage to `val`."
+  [key val]
+  (.setItem (.-localStorage js/window) key val))
+
+(defn get-item
+  "Returns value of `key' from browser's localStorage."
+  [key]
+  (.getItem (.-localStorage js/window) key))
+
+(defn remove-item!
+  "Remove the browser's localStorage value for the given `key`"
+  [key]
+  (.removeItem (.-localStorage js/window) key))
