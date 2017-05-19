eb6529b0d85d8f7a842d509672f4fdd1526c6f5e
Pavol Noha <pavol.noha@gmail.com>
2017-05-19 10:31:33 +0200

    cleanup -> upgrade figwheel

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojure/clojurescript "1.9.456"]
-                 [cljsjs/react "15.3.1-0"]
-                 [cljsjs/react-dom "15.3.1-0"]
-                 [cljsjs/material "1.3.0-0"]
-                 [rum "0.10.7" :exclusions [cljsjs/react cljsjs/react-dom]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+                 [org.clojure/clojurescript "1.9.542"]
+                 [cljsjs/react "15.4.2-2"]
+                 [cljsjs/react-dom "15.4.2-2"]
+                 [cljsjs/material-ui "0.18.1-0"]
+                 [rum "0.10.8" :exclusions [cljsjs/react cljsjs/react-dom]]
                  [ring "1.5.1"]
                  [ring/ring-json "0.4.0"]
                  [bidi "2.0.16"]
@@ -45,11 +45,6 @@
                 :source-paths ["src/cljs" "src/cljc"]
                 :figwheel     true
                 :compiler     {:main                 swarmpit.ui
-                               :foreign-libs         [{:file     "libs/material-ui/material-ui.inc.js"
-                                                       :file-min "libs/material-ui/material-ui.min.inc.js"
-                                                       :provides ["cljsjs.material-ui"]
-                                                       :requires ["cljsjs.react" "cljsjs.react.dom"]}]
-                               :externs              ["libs/material-ui/material-ui.ext.js"]
                                :preloads             [devtools.preload]
                                :asset-path           "js/out"
                                :output-to            "resources/public/js/main.js"
@@ -61,7 +56,6 @@
                 :jar          true
                 :compiler     {:main                 swarmpit.ui
                                :foreign-libs         [{:file     "libs/material-ui/material-ui.inc.js"
-                                                       :file-min "libs/material-ui/material-ui.min.inc.js"
                                                        :provides ["cljsjs.material-ui"]
                                                        :requires ["cljsjs.react" "cljsjs.react.dom"]}]
                                :externs              ["libs/material-ui/material-ui.ext.js"]
@@ -76,13 +70,13 @@
              :server-logfile "log/figwheel.log"}
 
   :profiles {:dev
-             {:dependencies [[figwheel "0.5.9"]
-                             [figwheel-sidecar "0.5.9"]
+             {:dependencies [[figwheel "0.5.10"]
+                             [figwheel-sidecar "0.5.10"]
                              [com.cemerick/piggieback "0.2.1"]
                              [org.clojure/tools.nrepl "0.2.12"]
                              [binaryage/devtools "0.9.2"]]
 
-              :plugins      [[lein-figwheel "0.5.9"]
+              :plugins      [[lein-figwheel "0.5.10"]
                              [lein-doo "0.1.6"]]
 
               :source-paths ["dev"]
