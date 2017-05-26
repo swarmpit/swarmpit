89b861fefa6846857516536f914fbe8188b5f682
Pavol Noha <pavol.noha@gmail.com>
2017-05-26 10:30:08 +0200

    format date, statusm cleanup

--- src/clj/swarmpit/docker/mapper/inbound.clj
+++ src/clj/swarmpit/docker/mapper/inbound.clj
@@ -1,6 +1,13 @@
 (ns swarmpit.docker.mapper.inbound
   "Map docker domain to swarmpit domain"
+  (:import (java.text SimpleDateFormat))
   (:require [clojure.string :as str]))
+
+(def date-format (SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ss"))
+
+(defn date
+  [date]
+  (str (.parse date-format date)))
 
 (defn ->node
   [node]
@@ -39,8 +46,8 @@
       :id id
       :taskName task-name
       :version (get-in task [:Version :Index])
-      :createdAt (get task :CreatedAt)
-      :updatedAt (get task :UpdatedAt)
+      :createdAt (date (get task :CreatedAt))
+      :updatedAt (date (get task :UpdatedAt))
       :image image-name
       :imageDigest image-digest
       :state (get-in task [:Status :State])
@@ -79,21 +86,26 @@
                  :value (second variable)})))
        (into [])))
 
+(defn ->service-replicas-running
+  [service-tasks]
+  (-> (filter #(= (get-in % [:Status :State]) "running") service-tasks)
+      (count)))
+
 (defn ->service-replicas-state
-  [replicas replicas-running service-mode]
+  [service-replicas service-replicas-running service-mode]
   (if (= service-mode "replicated")
-    (str replicas-running " / " replicas)
-    (str replicas-running " / " replicas-running)))
+    (str service-replicas-running " / " service-replicas)
+    (str service-replicas-running " / " service-replicas-running)))
 
 (defn ->service-state
-  [replicas replicas-running service-mode]
+  [service-replicas service-replicas-running service-mode]
   (case service-mode
-    "replicated" (if (zero? replicas-running)
+    "replicated" (if (zero? service-replicas-running)
                    "not running"
-                   (if (= replicas-running replicas)
+                   (if (= service-replicas-running service-replicas)
                      "running"
                      "partly running"))
-    "global" (if (zero? replicas-running)
+    "global" (if (zero? service-replicas-running)
                "not running"
                "running")))
 
@@ -102,11 +114,9 @@
   (let [service-mode (str/lower-case (name (first (keys (get-in service [:Spec :Mode])))))
         service-name (get-in service [:Spec :Name])
         service-id (get service :ID)
-        service-tasks (-> (filter #(= (:ServiceID %) service-id) tasks)
-                          (->tasks nodes service-name service-mode))
+        service-tasks (filter #(= (:ServiceID %) service-id) tasks)
         replicas (get-in service [:Spec :Mode :Replicated :Replicas])
-        replicas-running (-> (filter #(= (:state %) "running") service-tasks)
-                             (count))
+        replicas-running (->service-replicas-running service-tasks)
         image (get-in service [:Spec :TaskTemplate :ContainerSpec :Image])
         image-info (str/split image #"@")
         image-name (first image-info)
@@ -114,19 +124,20 @@
     (array-map
       :id service-id
       :version (get-in service [:Version :Index])
-      :createdAt (get service :CreatedAt)
-      :updatedAt (get service :UpdatedAt)
+      :createdAt (date (get service :CreatedAt))
+      :updatedAt (date (get service :UpdatedAt))
       :image image-name
       :imageDigest image-digest
       :serviceName service-name
       :mode service-mode
       :replicas replicas
-      :replicasState (->service-replicas-state replicas replicas-running service-mode)
       :state (->service-state replicas replicas-running service-mode)
+      :status {:info   (->service-replicas-state replicas replicas-running service-mode)
+               :update (some? (get-in service [:UpdateStatus :State]))}
       :ports (->service-ports service)
       :volumes (->service-volumes service)
       :variables (->service-variables service)
-      :tasks service-tasks)))
+      :tasks (->tasks service-tasks nodes service-name service-mode))))
 
 (defn ->services
   [services tasks nodes]
@@ -140,7 +151,7 @@
     (array-map
       :id (get network :Id)
       :networkName (get network :Name)
-      :created (get network :Created)
+      :created (date (get network :Created))
       :driver (get network :Driver)
       :internal (get network :Internal)
       :subnet (:Subnet config)
