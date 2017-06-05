2199b18063b19b68c5761637a74cba1e67f1a35d
Pavol Noha <pavol.noha@gmail.com>
2017-06-05 20:18:10 +0200

    user dev profile

---
+++ dev/script/init-db.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$(docker ps -q -f name=swarmpitdb)" ];
+   echo "Swarmpit DB already exist."
+then
+    if [ "$(docker ps -aq -f status=exited -f name=swarmpitdb)" ];
+    then
+        echo "Swarmpit DB down. Starting ..."
+        docker start swarmpitdb
+    else
+        echo "Swarmpit DB running."
+    fi
+else
+    echo "Creating swarmpit DB"
+    docker run -d -p 5984:5984 --name swarmpitdb klaemo/couchdb:latest
+fi
--- dev/user.clj
+++ dev/user.clj
@@ -1,7 +1,9 @@
 (ns user
-  (:require [swarmpit.server]
-            [ring.middleware.reload :refer [wrap-reload]]
-            [figwheel-sidecar.repl-api :as figwheel]))
+  (:use [clojure.java.shell :only [sh]])
+  (:require [ring.middleware.reload :refer [wrap-reload]]
+            [figwheel-sidecar.repl-api :as figwheel]
+            [swarmpit.api :as api]
+            [swarmpit.server]))
 
 ;; Let Clojure warn you when it needs to reflect on types, or when it does math
 ;; on unboxed numbers. In both cases you should add type annotations to prevent
@@ -9,11 +11,33 @@
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* :warn-on-boxed)
 
+(defn- init-user
+  []
+  (api/create-user {:username "admin"
+                    :password "admin"
+                    :email    "admin@admin.com"
+                    :role     "admin"}))
+
+(defn- init-registry
+  []
+  (api/create-registry {:name      "dockerhub"
+                        :version   "v1"
+                        :scheme    "https"
+                        :url       "registry.hub.docker.com"
+                        :isPrivate false}))
+
+(defn- on-startup
+  []
+  (print (:out (sh "sh" "dev/script/init-db.sh")))
+  (println (str "Swarmpit DB schema status: " (api/create-database)))
+  (println (str "Swarmpit DEV user status: " (or (init-user) "Admin user already exist")))
+  (println (str "Swarmpit DEV registry status: " (or (init-registry) "Dockerhub registry already exist"))))
+
 (def http-handler
   (wrap-reload #'swarmpit.server/app))
 
 (defn run []
-  (figwheel/start-figwheel!)
-  (swarmpit.server/on-startup))
+  (on-startup)
+  (figwheel/start-figwheel!))
 
 (def browser-repl figwheel/cljs-repl)
