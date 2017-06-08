59425f2c6da73b94cc22fd2cd2772650c2605d79
Pavol Noha <pavol.noha@gmail.com>
2017-06-08 14:11:05 +0200

    material-ui extern for advanced compilation

---
+++ ext/material-ui.ext.js
@@ -0,0 +1,109 @@
+var MaterialUI = {
+    ToolbarTitle: {},
+    ToolbarSeparator: {},
+    ToolbarGroup: {},
+    Toolbar: {},
+    Toggle: {},
+    TimePicker: {},
+    TextField: {},
+    TableRowColumn: {},
+    TableRow: {},
+    TableHeaderColumn: {},
+    TableHeader: {},
+    TableFooter: {},
+    TableBody: {},
+    Table: {},
+    Tab: {},
+    Tabs: {},
+    Snackbar: {},
+    Stepper: {},
+    StepLabel: {},
+    StepContent: {},
+    StepButton: {},
+    Step: {},
+    SvgIcon: {},
+    Subheader: {},
+    Slider: {},
+    SelectField: {},
+    RefreshIndicator: {},
+    RaisedButton: {},
+    RadioButtonGroup: {},
+    RadioButton: {},
+    Popover: {},
+    Paper: {},
+    MenuItem: {},
+    Menu: {},
+    ListItem: {},
+    List: {},
+    LinearProgress: {},
+    IconMenu: {},
+    IconButton: {},
+    GridTile: {},
+    GridList: {},
+    FontIcon: {},
+    FloatingActionButton: {},
+    FlatButton: {},
+    DropDownMenu: {},
+    Drawer: {},
+    Divider: {},
+    Dialog: {},
+    DatePicker: {},
+    CircularProgress: {},
+    Chip: {},
+    Checkbox: {},
+    CardText: {},
+    CardTitle: {},
+    CardMedia: {},
+    CardHeader: {},
+    CardActions: {},
+    Card: {},
+    BottomNavigationItem: {},
+    BottomNavigation: {},
+    Badge: {},
+    Avatar: {},
+    AutoComplete: {
+        noFilter: {},
+        defaultFilter: {},
+        caseInsensitiveFilter: {},
+        levenshteinDistanceFilter: {},
+        fuzzyFilter: {}
+    },
+    AppBar: {}
+};
+
+var MaterialUIStyles = {
+    MuiThemeProvider : function() {},
+    darkBaseTheme: function() {},
+    lightBaseTheme: function() {},
+    getMuiTheme : function() {},
+    themeManager: {
+        getMuiTheme: function() {},
+        modifyRawThemeFontFamily: function() {},
+        modifyRawThemePalette: function() {},
+        modifyRawThemeSpacing: function() {}
+    },
+    transitions : {
+        create : function() {},
+        easeOut  : function() {}
+    }
+};
+
+var MaterialUIUtils = {
+    withWidth : {
+        SMALL : {},
+        MEDIUM : {},
+        LARGE : {},
+        "default" : function() {}
+    },
+    colorManipulator : {
+        convertColorToString : function() {},
+        convertHexToRGB : function() {},
+        darken : function() {},
+        decomposeColor : function() {},
+        emphasize : function() {},
+        fade : function() {},
+        getContrastRatio : function() {},
+        getLuminance : function() {},
+        lighten : function() {}
+    }
+};
