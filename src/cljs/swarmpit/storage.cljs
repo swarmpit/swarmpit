5dd970696d6fe1f81a744a624eee74ae6d6f7f8d
Pavol Noha <pavol.noha@gmail.com>
2017-06-15 23:03:46 +0200

    storage helpers

--- src/cljs/swarmpit/storage.cljs
+++ src/cljs/swarmpit/storage.cljs
@@ -1,5 +1,6 @@
 (ns swarmpit.storage
-  (:refer-clojure :exclude [get remove]))
+  (:refer-clojure :exclude [get remove])
+  (:require [swarmpit.token :as token]))
 
 (def storage (.-localStorage js/window))
 
@@ -17,3 +18,21 @@
   "Remove value from browser's localStorage by given `key`"
   [key]
   (.removeItem storage key))
+
+(defn claims
+  []
+  (->> (get "token")
+       (token/decode-jwt)))
+
+(defn user
+  []
+  (get-in (claims) [:usr :username]))
+
+(defn email
+  []
+  (get-in (claims) [:usr :email]))
+
+(defn admin?
+  []
+  (= "admin"
+     (get-in (claims) [:usr :role])))
