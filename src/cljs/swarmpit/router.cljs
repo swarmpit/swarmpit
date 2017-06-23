4f61167de8256ea161aa831291e8f9419c635e15
Pavol Noha <pavol.noha@gmail.com>
2017-06-23 20:05:06 +0200

    routing to login if no token

--- src/cljs/swarmpit/router.cljs
+++ src/cljs/swarmpit/router.cljs
@@ -1,13 +1,16 @@
 (ns swarmpit.router
   (:require [bidi.router :as br]
             [swarmpit.routes :as routes]
+            [swarmpit.storage :as storage]
             [swarmpit.controller :as controller]))
 
 (defonce location (atom nil))
 
 (defn- on-navigate
   [location]
-  (controller/dispatch location))
+  (if (nil? (storage/get "token"))
+    (controller/dispatch {:handler :login})
+    (controller/dispatch location)))
 
 (defn start
   []
