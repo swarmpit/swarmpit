f2ebf05ddb000c2b2683785b3ce3735b70a0594e
Pavol Noha <pavol.noha@gmail.com>
2017-06-25 23:42:49 +0200

    version changed from 0.1.0 to 1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject swarmpit "0.1.0-SNAPSHOT"
+(defproject swarmpit "1.0-SNAPSHOT"
   :description "Docker swarm management UI"
   :url "http://swarmpit.io"
   :license {:name "Eclipse Public License"
