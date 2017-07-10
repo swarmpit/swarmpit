194fc8e54210e7071fd512915cc44ce85b96ad73
lumir-mrkva <lumir.mrkva@topmonks.com>
2017-07-10 23:31:23 +0200

    don't deploy pull-requests

--- dev/script/deploy-image.sh
+++ dev/script/deploy-image.sh
@@ -3,16 +3,19 @@ ORG=swarmpit
 REPO=swarmpit
 NAMESPACE=$ORG/$REPO
 
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker tag $REPO "$NAMESPACE:build-$TRAVIS_BUILD_NUMBER"
-docker tag $REPO "$NAMESPACE:sha-$TRAVIS_COMMIT"
 if [ $TRAVIS_PULL_REQUEST == "false" ]
 then
-		docker tag $REPO "$NAMESPACE:$TRAVIS_BRANCH"
+	docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+	docker tag $REPO "$NAMESPACE:build-$TRAVIS_BUILD_NUMBER"
+	docker tag $REPO "$NAMESPACE:sha-$TRAVIS_COMMIT"
+	docker tag $REPO "$NAMESPACE:$TRAVIS_BRANCH"
 
 		if [ $TRAVIS_BRANCH == "master" ]
 		then
 			docker tag $REPO "$NAMESPACE:latest"
 		fi
+	
+	docker push "$NAMESPACE"
+else
+	echo "pull-requests are not deployed"
 fi
-docker push "$NAMESPACE"
