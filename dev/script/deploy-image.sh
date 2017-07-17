04db18216fc69bcd0a853d134936be21ddb7851f
lumir-mrkva <lumir.mrkva@topmonks.com>
2017-07-17 12:41:57 +0200

    pushing pr images as :pr-#

--- dev/script/deploy-image.sh
+++ dev/script/deploy-image.sh
@@ -2,18 +2,25 @@
 ORG=swarmpit
 REPO=swarmpit
 NAMESPACE=$ORG/$REPO
+BRANCH=${TRAVIS_BRANCH/\//-}
 
-if [ $TRAVIS_PULL_REQUEST == "false" ]
+if [ $CONTRIBUTOR == "true" ] 
 then
 	docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-	docker tag $REPO "$NAMESPACE:$TRAVIS_BRANCH"
+	
+	if [ $TRAVIS_PULL_REQUEST == "false" ]
+	then
+		docker tag $REPO "$NAMESPACE:$BRANCH"
 
-		if [ $TRAVIS_BRANCH == "master" ]
-		then
-			docker tag $REPO "$NAMESPACE:latest"
-		fi
+			if [ $BRANCH == "master" ]
+			then
+				docker tag $REPO "$NAMESPACE:latest"
+			fi
+	else
+		docker tag $REPO "$NAMESPACE:pr-$TRAVIS_PULL_REQUEST"
+	fi
 	
 	docker push "$NAMESPACE"
 else
-	echo "pull-requests are not deployed"
+	echo "images can be pushed only from base repo"
 fi
