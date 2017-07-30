699cfb65f6b6ca3bb6f78acb90cd44565fa0ecb0
Pavol Noha <pavol.noha@gmail.com>
2017-07-30 20:12:39 +0200

    cleanup -> authentication + authorization in separate ns

--- src/clj/swarmpit/server.clj
+++ src/clj/swarmpit/server.clj
@@ -5,60 +5,17 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.resource :refer [wrap-resource]]
             [ring.middleware.gzip :refer [wrap-gzip]]
-            [buddy.auth.backends.token :refer [jws-backend]]
-            [buddy.auth :refer [authenticated?]]
-            [buddy.auth.accessrules :refer [success error wrap-access-rules]]
-            [buddy.auth.middleware :refer [authentication-request]]
+            [swarmpit.authentication :refer [wrap-authentication]]
+            [swarmpit.authorization :refer [wrap-authorization]]
+            [swarmpit.handler :as handler :refer :all]
             [org.httpkit.server :refer [run-server]]
             [cheshire.core :refer [parse-string]]
             [bidi.ring :refer [make-handler]]
+            [bidi.bidi :refer [match-pair match-route*]]
             [clojure.tools.logging :as log]
-            [clojure.string :refer [starts-with?]]
-            [swarmpit.handler :as handler :refer :all]
             [swarmpit.routes :as routes]
             [swarmpit.install :as install]
-            [swarmpit.agent :as agent]
-            [swarmpit.couchdb.client :as cc]))
-
-(defn authentication-backend
-  [secret]
-  (jws-backend
-    {:secret     secret
-     :token-name "Bearer"
-     :on-error   (fn [_ ex] (throw ex))}))
-
-(defn authenticated-access
-  [request]
-  (if (authenticated? request)
-    true
-    (error {:code    401
-            :message "Authentication failed"})))
-
-(defn any-access
-  [_]
-  true)
-
-(defn admin-access
-  [request]
-  (let [role (get-in (:identity request) [:usr :role])]
-    (if (= "admin" role)
-      true
-      (error {:code    403
-              :message "Unauthorized access"}))))
-
-(def rules [{:pattern #"^/admin/.*"
-             :handler {:and [authenticated-access admin-access]}}
-            {:pattern #"^/login$"
-             :handler any-access}
-            {:pattern #"^/$"
-             :handler any-access}
-            {:pattern #"^/.*"
-             :handler authenticated-access}])
-
-(defn rules-error
-  [_ val]
-  (resp-error (:code val)
-              (:message val)))
+            [swarmpit.agent :as agent]))
 
 (defn wrap-client-exception
   [handler]
@@ -77,31 +34,24 @@
         {:status 500
          :body   (Throwable->map e)}))))
 
-(defn wrap-authentication
+(defn wrap-path-params
   [handler]
-  (fn [request]
-    (let [secret (:secret (cc/get-secret))
-          auth-backend (authentication-backend secret)]
-      (try
-        (handler (authentication-request request auth-backend))
-        (catch ExceptionInfo ex
-          (let [error (case (:cause (ex-data ex))
-                        :exp "Token expired"
-                        :signature "Token corrupted"
-                        "Token invalid")]
-            (resp-unauthorized error)))))))
+  (fn [{:keys [uri] :as request}]
+    (let [{:keys [route-params]} (match-route* routes/backend uri request)]
+      (handler
+        (assoc request :path-params route-params)))))
 
 (def app
   (-> (make-handler routes/backend handler/dispatch)
       (wrap-resource "public")
       (wrap-resource "react")
-      (wrap-access-rules {:rules    rules
-                          :on-error rules-error})
-      wrap-authentication
+      wrap-authorization
       wrap-client-exception
-      wrap-json-response
-      wrap-json-params
       wrap-fallback-exception
+      wrap-authentication
+      wrap-path-params
+      wrap-json-params
+      wrap-json-response
       (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))
       wrap-gzip))
 
