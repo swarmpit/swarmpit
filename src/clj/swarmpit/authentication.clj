7de5b77b1fc8f2eed7281295de5ae12926514ad3
Pavol Noha <pavol.noha@gmail.com>
2017-07-30 20:10:02 +0200

    auth ns

---
+++ src/clj/swarmpit/authentication.clj
@@ -0,0 +1,27 @@
+(ns swarmpit.authentication
+  (:import (clojure.lang ExceptionInfo))
+  (:require [buddy.auth.backends.token :refer [jws-backend]]
+            [buddy.auth.middleware :refer [authentication-request]]
+            [swarmpit.handler :refer [resp-unauthorized]]
+            [swarmpit.couchdb.client :as cc]))
+
+(defn- authentication-backend
+  [secret]
+  (jws-backend
+    {:secret     secret
+     :token-name "Bearer"
+     :on-error   (fn [_ ex] (throw ex))}))
+
+(defn wrap-authentication
+  [handler]
+  (fn [request]
+    (let [secret (:secret (cc/get-secret))
+          auth-backend (authentication-backend secret)]
+      (try
+        (handler (authentication-request request auth-backend))
+        (catch ExceptionInfo ex
+          (let [error (case (:cause (ex-data ex))
+                        :exp "Token expired"
+                        :signature "Token invalid"
+                        (Throwable->map ex))]
+            (resp-unauthorized error)))))))
