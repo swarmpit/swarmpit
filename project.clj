41d962f032a2dc5dfe1e67bc41db641ef981f27e
Pavol Noha <pavol.noha@gmail.com>
2017-08-21 18:59:01 +0200

    prepare for next iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject swarmpit "1.1-SNAPSHOT"
+(defproject swarmpit "1.2-SNAPSHOT"
   :description "Docker swarm management UI"
   :url "http://swarmpit.io"
   :license {:name "Eclipse Public License"
