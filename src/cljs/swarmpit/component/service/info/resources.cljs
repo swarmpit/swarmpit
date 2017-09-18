19590f4fb1450451921785202499714125c6ab16
Pavol Noha <pavol.noha@gmail.com>
2017-09-18 19:49:32 +0200

    resource info form #134

---
+++ src/cljs/swarmpit/component/service/info/resources.cljs
@@ -0,0 +1,12 @@
+(ns swarmpit.component.service.info.resources
+  (:require [material.component :as comp]
+            [rum.core :as rum]))
+
+(rum/defc form < rum/static [{:keys [limit reservation]}]
+  [:div.form-service-view-group.form-service-group-border
+   (comp/form-section "Reservation")
+   (comp/form-item "CPU" (:cpu reservation))
+   (comp/form-item "MEMORY" (str (:memory reservation) " MB"))
+   (comp/form-section "Limit")
+   (comp/form-item "CPU" (:cpu limit))
+   (comp/form-item "MEMORY" (str (:memory limit) " MB"))])
