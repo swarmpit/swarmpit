7817bb866c2a27022a53fbeb948a099302c1ed56
Lumir Mrkva <lumir.mrkva@topmonks.com>
2017-09-28 23:32:39 +0200

    wait for db on start, fixes #96

--- src/clj/swarmpit/install.clj
+++ src/clj/swarmpit/install.clj
@@ -2,6 +2,25 @@
   (:require [swarmpit.couchdb.client :as cc]
             [swarmpit.couchdb.migration :refer [migrate]]))
 
+(defn- db-ready?
+  []
+  (try (cc/db-version)
+       (catch Exception ex false)))
+
+(defn- wait-for-db
+  [sec]
+  (println "Waiting for DB...")
+  (loop [n sec]
+    (if (db-ready?)
+      (println (str "... connected after " (- sec n) " sec"))
+      (if (zero? n)
+        (do
+          (println "... timeout")
+          (throw (ex-info "DB timeout" nil)))
+        (do
+          (Thread/sleep 1000)
+          (recur (dec n)))))))
+
 (defn- create-database
   []
   (try
@@ -11,7 +30,7 @@
 
 (defn init
   []
-  (cc/db-version)
+  (wait-for-db 100)
   (when (not (:error (create-database)))
     (println "DB schema created"))
   (migrate))
