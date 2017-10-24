f3be5b9b71b78b2d966e269f44666749b88f3312
Pavol Noha <pavol.noha@gmail.com>
2017-10-24 16:12:10 +0200

    resources form cleanup -> sliders for cpu

--- src/cljs/swarmpit/component/service/form_resources.cljs
+++ src/cljs/swarmpit/component/service/form_resources.cljs
@@ -1,5 +1,6 @@
 (ns swarmpit.component.service.form-resources
   (:require [material.component :as comp]
+            [material.icon :as icon]
             [swarmpit.component.parser :refer [parse-int parse-float]]
             [swarmpit.component.state :as state]
             [sablono.core :refer-macros [html]]
@@ -9,21 +10,23 @@
 
 (def cursor [:page :service :wizard :resources])
 
+(defn- cpu-value
+  [value]
+  (if (zero? value)
+    "unlimited"
+    value))
+
 (defn- form-cpu-reservation [value]
   (comp/form-comp
-    "CPU"
-    (comp/vtext-field
-      {:name            "cpu-reservation"
-       :key             "cpu-reservation"
-       :type            "number"
-       :min             0.000
-       :step            0.001
-       :max             1.0
-       :validations     "isValidCPUValue"
-       :validationError "Please use maximum of 1.0 (represents a whole CPU) or leave blank for default value"
-       :value           value
-       :onChange        (fn [_ v]
-                          (state/update-value [:reservation :cpu] (parse-float v) cursor))})))
+    (str "CPU  " "(" (cpu-value value) ")")
+    (comp/slider #js {:min          0
+                      :max          2
+                      :step         0.10
+                      :defaultValue 0
+                      :value        value
+                      :onChange     (fn [_ v]
+                                      (state/update-value [:reservation :cpu] (parse-float v) cursor))
+                      :sliderStyle  #js {:marginTop "14px"}})))
 
 (defn- form-memory-reservation [value]
   (comp/form-comp
@@ -34,26 +37,22 @@
        :type            "number"
        :min             4
        :validations     "isValidMemoryValue"
-       :validationError "Please use minimum of 4 MB or leave blank for default value"
+       :validationError "Please use minimum of 4 MB or leave blank for unlimited"
        :value           value
        :onChange        (fn [_ v]
                           (state/update-value [:reservation :memory] (parse-int v) cursor))})))
 
 (defn- form-cpu-limit [value]
   (comp/form-comp
-    "CPU"
-    (comp/vtext-field
-      {:name            "cpu-limit"
-       :key             "cpu-limit"
-       :type            "number"
-       :min             0.000
-       :step            0.001
-       :max             1.0
-       :validations     "isValidCPUValue"
-       :validationError "Please use maximum of 1.0 (represents a whole CPU) or leave blank for default value"
-       :value           value
-       :onChange        (fn [_ v]
-                          (state/update-value [:limit :cpu] (parse-float v) cursor))})))
+    (str "CPU  " "(" (cpu-value value) ")")
+    (comp/slider #js {:min          0
+                      :max          2
+                      :step         0.10
+                      :defaultValue 0
+                      :value        value
+                      :onChange     (fn [_ v]
+                                      (state/update-value [:limit :cpu] (parse-float v) cursor))
+                      :sliderStyle  #js {:marginTop "14px"}})))
 
 (defn- form-memory-limit [value]
   (comp/form-comp
@@ -64,7 +63,7 @@
        :type            "number"
        :min             4
        :validations     "isValidMemoryValue"
-       :validationError "Please use minimum of 4 MB or leave blank for default value"
+       :validationError "Please use minimum of 4 MB or leave blank for unlimited"
        :value           value
        :onChange        (fn [_ v]
                           (state/update-value [:limit :memory] (parse-int v) cursor))})))
@@ -77,9 +76,11 @@
        {:onValid   #(state/update-value [:isValid] true cursor)
         :onInvalid #(state/update-value [:isValid] false cursor)}
        (html (comp/form-subsection "Reservation"))
+       (html (comp/form-icon-value icon/info [:span "Minimal resource availablility to run a task. Empty for unlimited."]))
        (form-cpu-reservation (:cpu reservation))
        (form-memory-reservation (:memory reservation))
        (html (comp/form-subsection "Limit"))
+       (html (comp/form-icon-value icon/info [:span "Maximal resource usage per task. Empty for unlimited."]))
        (form-cpu-limit (:cpu limit))
        (form-memory-limit (:memory limit))
        (html [:div {:style {:height "20px"}}]))]))
