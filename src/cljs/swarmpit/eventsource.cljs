23c5f053e856b2185f9fec322aeb6ced2fbb5a82
Pavol Noha <pavol.noha@gmail.com>
2017-10-30 19:49:34 +0100

    parse event and prepare for handling #165

--- src/cljs/swarmpit/eventsource.cljs
+++ src/cljs/swarmpit/eventsource.cljs
@@ -1,25 +1,40 @@
 (ns swarmpit.eventsource
-  (:require [swarmpit.routes :as routes]))
+  (:require [swarmpit.routes :as routes]
+            [clojure.walk :refer [keywordize-keys]]))
 
-(defn subscribe
-  [uri]
-  (js/EventSource. uri))
+(defn parse-event
+  [event]
+  (-> (.-data event)
+      (js/JSON.parse)
+      (js->clj)
+      (keywordize-keys)))
 
-(defn add-listener!
-  [evs eventname callback]
-  (.addEventListener evs eventname callback))
+(defn handle-docker-event!
+  [event-message]
+  "TODO: Handler docker events"
+  (print event-message))
+
+(defn handle-event!
+  [event-data]
+  (let [event-source (:From event-data)
+        event-message (:Message event-data)]
+    (case event-source
+      "DOCKER" (handle-docker-event! event-message)
+      (print (str "Unknown event source: " event-source)))))
 
 (defn init!
   []
-  (let [event-source (subscribe (routes/path-for-backend :events))]
-    (add-listener! event-source "message"
-                   (fn [event]
-                     (let [data (.-data event)]
-                       (print data))))
-    (add-listener! event-source "open"
-                   (fn [_]
-                     (print "Swarmpit event connection was opened")))
-    (add-listener! event-source "error"
-                   (fn [event]
-                     (let [state (.-readyState event)]
-                       (print state))))))
+  (let [event-source (js/EventSource. (routes/path-for-backend :events))]
+    (.addEventListener event-source "message"
+                       (fn [event]
+                         (let [event-data (parse-event event)]
+                           (handle-event! event-data))))
+    (.addEventListener event-source "open"
+                       (fn [_]
+                         (print "Swarmpit event connection was opened")))
+    (.addEventListener event-source "error"
+                       (fn [event]
+                         (let [state (.-readyState event)]
+                           (print state))))))
+
+
