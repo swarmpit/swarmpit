c31e2d84f77b952b4d40bee94eae0801819cab95
Pavol Noha <pavol.noha@gmail.com>
2017-11-16 21:38:57 +0100

    if event connection error try reconnect in 5 seconds #165

--- src/cljs/swarmpit/event/source.cljs
+++ src/cljs/swarmpit/event/source.cljs
@@ -1,5 +1,7 @@
 (ns swarmpit.event.source
-  (:require [swarmpit.routes :as routes]
+  (:require [yaffle.es]
+            [swarmpit.routes :as routes]
+            [swarmpit.storage :as storage]
             [swarmpit.component.state :as state]
             [swarmpit.event.docker :as docker-event]
             [clojure.walk :refer [keywordize-keys]]))
@@ -17,6 +19,13 @@
   (let [route (state/get-value [:route])]
     (docker-event/handle route event-message)))
 
+(defn handle-swarmpit-event!
+  [event-message]
+  "Handler events from swarmpit be"
+  (when (not= "heartbeat" event-message)
+    ;; If no heartbeat handle event
+    ))
+
 (defn handle-event!
   [event-data]
   "Handler events"
@@ -24,20 +33,25 @@
         event-message (:Message event-data)]
     (case event-source
       "DOCKER" (handle-docker-event! event-message)
+      "SWARMPIT" (handle-swarmpit-event! event-message)
       (print (str "Unknown event source: " event-source)))))
+
+(defn headers
+  []
+  (->> {:headers {"Authorization" (storage/get "token")}}
+       (clj->js)))
 
 (defn init!
   []
-  (let [event-source (js/EventSource. (routes/path-for-backend :events))]
+  (let [event-source (js/EventSourcePolyfill. (routes/path-for-backend :events) (headers))]
     (.addEventListener event-source "message"
                        (fn [event]
-                         (print (.-origin event))
                          (let [event-data (parse-event event)]
                            (handle-event! event-data))))
     (.addEventListener event-source "open"
                        (fn [_]
-                         (print "Swarmpit event connection was opened")))
+                         (print "Swarmpit event connection has been opened")))
     (.addEventListener event-source "error"
-                       (fn [event]
-                         (let [state (.-readyState event)]
-                           (print state))))))
+                       (fn [_]
+                         (print "Swarmpit event connection failed. Reconnecting in 5 sec...")
+                         (js/setTimeout #(init!) 5000)))))
