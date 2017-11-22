3584a36d03e507607c44dbd4febd16e2f44a6b2a
Pavol Noha <pavol.noha@gmail.com>
2017-11-22 18:36:48 +0100

    broadcast swarm scoped messages only once to FE

--- src/clj/swarmpit/handler_events.clj
+++ src/clj/swarmpit/handler_events.clj
@@ -1,6 +1,7 @@
 (ns swarmpit.handler-events
-  (:require [org.httpkit.server :refer [run-server with-channel on-close send! close]]
+  (:require [org.httpkit.server :refer [with-channel on-close send!]]
             [clojure.walk :refer [keywordize-keys]]
+            [clojure.core.memoize :as memo]
             [cheshire.core :refer [generate-string]]
             [immutant.scheduling :refer :all]
             [swarmpit.slt :as slt]
@@ -9,9 +10,16 @@
 (def channel-hub (atom {}))
 
 (defn broadcast [data]
+  "Broadcast message to all channels and return message hash."
   (let [message (str "data: " (generate-string data) "\n\n")]
     (doseq [channel (keys @channel-hub)]
-      (send! channel message false))))
+      (send! channel message false))
+    (hash data)))
+
+;; Swarm scoped events are received from each manager, but
+;; we want broadcast to FE only once to prevent duplicity.
+;; Therefore we cache event message hash for 1 second.
+(def broadcast-memo (memo/ttl broadcast :ttl/threshold 1000))
 
 (defmethod dispatch :events [_]
   (fn [{:keys [query-params] :as request}]
@@ -32,6 +40,6 @@
   (fn [{:keys [params]}]
     (if (some? params)
       (do
-        (broadcast params)
+        (broadcast-memo params)
         (resp-accepted (str "Broadcasted to " (count @channel-hub) " clients")))
-      (resp-error 400 "No data send"))))
+      (resp-error 400 "No data sent"))))
