e7cfc664da5ad6a96598fb783cbba222a10218a2
Lumir Mrkva <lumir.mrkva@topmonks.com>
2017-12-30 20:10:39 +0100

    removed sleep, app is already waiting for db

--- dev/script/init-db.sh
+++ dev/script/init-db.sh
@@ -7,12 +7,10 @@ then
     then
         echo "Swarmpit DB down. Starting ..."
         docker start swarmpitdb
-        sleep 5
     else
         echo "Swarmpit DB running."
     fi
 else
     echo "Creating swarmpit DB"
     docker run -d -p 5984:5984 --name swarmpitdb klaemo/couchdb:latest
-    sleep 10
 fi
