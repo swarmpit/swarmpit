11accaada5ffa3f9dd0cf2e73a7ec8c567c17ffc
Pavol Noha <pavol.noha@gmail.com>
2018-01-20 14:16:34 +0100

    cleanup agent

--- src/clj/swarmpit/agent.clj
+++ src/clj/swarmpit/agent.clj
@@ -13,21 +13,19 @@
     (doseq [service services]
       (let [id (:id service)
             repository (:repository service)]
-        (log/info repository)
         (try
           (let [current-digest (:imageDigest repository)
-                latest-digest (api/repository-digest nil (:name repository) (:tag repository))]
+                latest-digest (api/repository-digest nil
+                                                     (:name repository)
+                                                     (:tag repository))]
             (when (not= current-digest
                         latest-digest)
               (api/redeploy-service nil id)
-              (log/info "Service" id "has been redeployed! [" current-digest "] -> [" latest-digest "]")))
+              (log/info "Service" id "autoredeploy fired! DIGEST: [" current-digest "] -> [" latest-digest "]")))
           (catch ExceptionInfo e
             (log/error "Service" id "autoredeploy failed! " (dissoc (ex-data e) :headers))))))))
 
 (defn init []
   (schedule-task 60000
-                 (try
-                   (autoredeploy-job)
-                   (catch Exception ex
-                     (log/error "Autoredeploy execution failure:" ex)))
+                 (autoredeploy-job)
                  (init)))
