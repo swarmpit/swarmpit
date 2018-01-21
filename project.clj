51168103fd4ae240347d0cedf9571fc6f3232d28
Pavol Noha <pavol.noha@gmail.com>
2018-01-21 20:40:29 +0100

    rel. 1.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject swarmpit "1.4-SNAPSHOT"
+(defproject swarmpit "1.3.1"
   :description "Docker swarm management UI"
   :url "http://swarmpit.io"
   :license {:name "Eclipse Public License"
