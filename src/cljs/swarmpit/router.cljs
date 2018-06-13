34194d01dd8816bf9c7643b44fa830ca57458357
Lumir Mrkva <lumir.mrkva@topmonks.com>
2018-06-13 13:39:08 +0200

    stack is now default index page (atleast till dashboard page is finished)

--- src/cljs/swarmpit/router.cljs
+++ src/cljs/swarmpit/router.cljs
@@ -34,7 +34,7 @@
   [location]
   ;; Render to service list by default as we don't have any index page right now
   (if (= :index (:handler location))
-    (set-route {:handler :service-list})
+    (set-route {:handler :stack-list})
     (set-route location)))
 
 (defn start
