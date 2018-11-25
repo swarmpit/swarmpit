ac715b9b304f075039e6b91c34b89355195b90a2
Pavol Noha <pavol.noha@gmail.com>
2018-11-25 19:00:40 +0100

    cleaup progress

--- src/cljs/swarmpit/component/progress.cljs
+++ src/cljs/swarmpit/component/progress.cljs
@@ -1,41 +1,16 @@
 (ns swarmpit.component.progress
   (:require [material.component :as comp]
-            [material.component.form :as form]
+            [sablono.core :refer-macros [html]]
             [rum.core :as rum]))
 
 (enable-console-print!)
 
-(def progress-style
-  {:background "rgba(0, 0, 0, .2)"
-   :position   "absolute"
-   :display    "table"
-   :textAlign  "center"
-   :top        0
-   :left       0
-   :height     "100%"
-   :width      "100%"
-   :zIndex     10000})
-
-(def progress-inner-style
-  {:display       "table-cell"
-   :verticalAlign "middle"})
-
-(rum/defc progress < rum/static []
-  (comp/mui
-    (comp/circular-progress
-      {:color      "#fff"
-       :size       80
-       :thickness  8
-       :style      progress-style
-       :innerStyle progress-inner-style})))
-
 (rum/defc form < rum/static [loading? comp]
   (if loading?
-    (progress)
-    comp))
-
-(rum/defc loading < rum/static []
-  (form/loading true))
-
-(rum/defc loaded < rum/static []
-  (form/loading false))
+    (html
+      [:div {:style {:width          "100%"
+                     :height         "100vh"
+                     :display        "flex"
+                     :alignItems     "center"
+                     :justifyContent "center"}}
+       [:div {:style {:maxWidth "50%"}} (comp/circular-progress {:size 100})]]) comp))
