27f7358dd811324babc898331a465ad14b0b0fae
Pavol Noha <pavol.noha@gmail.com>
2018-12-05 11:55:30 +0100

    deployment section styling

--- src/cljs/swarmpit/component/service/info/deployment.cljs
+++ src/cljs/swarmpit/component/service/info/deployment.cljs
@@ -1,18 +1,16 @@
 (ns swarmpit.component.service.info.deployment
-  (:require [material.icon :as icon]
-            [material.components :as comp]
+  (:require [material.components :as comp]
             [material.component.form :as form]
             [sablono.core :refer-macros [html]]
             [rum.core :as rum]))
 
 (enable-console-print!)
 
-(def placement-render-item-keys
-  [[:rule]])
-
-(defn- placement-render-item
-  [item]
-  (val item))
+(defn item [name value]
+  (html
+    [:div.Swarmoit-row-space
+     [:span name]
+     [:span value]]))
 
 (rum/defc form < rum/static [deployment]
   (let [autoredeploy (:autoredeploy deployment)
@@ -29,81 +27,84 @@
         restart-policy-delay (get-in deployment [:restartPolicy :delay])
         restart-policy-window (get-in deployment [:restartPolicy :window])
         restart-policy-attempts (get-in deployment [:restartPolicy :attempts])]
-
-
     (comp/card
       {:className "Swarmpit-form-card"}
       (comp/card-header
         {:className "Swarmpit-form-card-header"
-         :subheader (form/subheader "Deployment" icon/settings)})
+         :title     "Deployment"})
       (comp/card-content
         {}
-        (html
-          [:div {:style {:display        "flex"
-                         :flexDirection  "row"
-                         :justifyContent "space-between"}}
-           [:div
-            [:span "Restart policy"]
-            [:table
-             [:tr
-              [:td "CONDITION"]
-              [:td {:style {:textAlign "right"}} restart-policy-condition]]
-             [:tr
-              [:td "DELAY"]
-              [:td (str restart-policy-delay "s")]]
-             [:tr
-              [:td "WINDOW"]
-              [:td (str restart-policy-window "s")]]
-             [:tr
-              [:td "MAX ATTEMPTS"]
-              [:td restart-policy-attempts]]]]
-           [:div
-            [:span "Update policy"]
-            [:table
-             [:tr
-              [:td "PARALLELISM"]
-              [:td update-parallelism]]
-             [:tr
-              [:td "DELAY"]
-              [:td (str update-delay "s")]]
-             [:tr
-              [:td "ORDER"]
-              [:td update-order]]
-             [:tr
-              [:td "ON FAILURE"]
-              [:td update-failure-action]]]]])))))
-
-
-
-
-
-
-    ;[:div.form-layout-group.form-layout-group-border
-    ; (form/subsection "Deployment")
-    ; (form/item "AUTOREDEPLOY" (if autoredeploy
-    ;                             "on"
-    ;                             "off"))
-    ; (if (not-empty placement)
-    ;   [:div
-    ;    (form/subsection "Placement")
-    ;    (list/table-headless placement
-    ;                         placement-render-item
-    ;                         placement-render-item-keys)])
-    ; (form/subsection "Restart Policy")
-    ; (form/item "CONDITION" restart-policy-condition)
-    ; (form/item "DELAY" (str restart-policy-delay "s"))
-    ; (form/item "WINDOW" (str restart-policy-window "s"))
-    ; (form/item "MAX ATTEMPTS" restart-policy-attempts)
-    ; (form/subsection "Update Config")
-    ; (form/item "PARALLELISM" update-parallelism)
-    ; (form/item "DELAY" (str update-delay "s"))
-    ; (form/item "ORDER" update-order)
-    ; (form/item "ON FAILURE" update-failure-action)
-    ; (if (= "rollback" update-failure-action)
-    ;   [:div
-    ;    (form/subsection "Rollback Config")
-    ;    (form/item "PARALLELISM" rollback-parallelism)
-    ;    (form/item "DELAY" (str rollback-delay "s"))
-    ;    (form/item "ORDER" rollback-order)
-    ;    (form/item "ON FAILURE" rollback-failure-action)])]
+        (comp/grid
+          {:container true
+           :spacing   40}
+          (comp/grid
+            {:item true
+             :xs   6}
+            (item "Autoredeploy" (str autoredeploy)))
+          (comp/grid
+            {:item true
+             :xs   12}
+            (form/subsection "Placements")
+            (map #(comp/chip {:label (:rule %)}) placement))
+          (comp/grid
+            {:item true
+             :xs   12
+             :sm   6}
+            (form/subsection "Restart Policy")
+            (comp/grid
+              {:container true
+               :direction "column"}
+              (comp/grid
+                {:item true}
+                (item "Condition" restart-policy-condition))
+              (comp/grid
+                {:item true}
+                (item "Delay" (str restart-policy-delay "s")))
+              (comp/grid
+                {:item true}
+                (item "Window" (str restart-policy-window "s")))
+              (comp/grid
+                {:item true}
+                (item "Max Attempts" restart-policy-attempts))))
+          (comp/grid
+            {:item true
+             :xs   12
+             :sm   6}
+            (form/subsection "Update Config")
+            (comp/grid
+              {:container true
+               :direction "column"}
+              (comp/grid
+                {:item true}
+                (item "Parallelism" update-parallelism))
+              (comp/grid
+                {:item true}
+                (item "Delay" (str update-delay "s")))
+              (comp/grid
+                {:item true}
+                (item "Order" update-order))
+              (comp/grid
+                {:item true}
+                (item "On Failure" update-failure-action))))
+          (when (= "rollback" (:failureAction update))
+            (comp/grid
+              {:item true
+               :xs   12
+               :sm   6}
+              (form/subsection "Rollback Config")
+              (comp/grid
+                {:container true
+                 :direction "column"}
+                (comp/grid
+                  {:item true}
+                  (item "Parallelism" rollback-parallelism))
+                (comp/grid
+                  {:item true}
+                  (item "Delay" (str rollback-delay "s")))
+                (comp/grid
+                  {:item true}
+                  (item "Order" rollback-order))
+                (comp/grid
+                  {:item true}
+                  (item "On Failure" rollback-failure-action))))))))))
 
