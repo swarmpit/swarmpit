2570cc1189d297202e94abe8ffe154c841c5c204
Lumir Mrkva <lumir.mrkva@topmonks.com>
2019-07-29 14:17:48 +0200

    force enable tcp api in dind for 19+
    related #380

--- dev/script/start-dind.sh
+++ dev/script/start-dind.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker run --privileged --name dind -p 12375:2375 -d docker:$DOCKER-dind
+docker run --privileged --name dind -p 12375:2375 -e DOCKER_TLS_CERTDIR='' -d docker:$DOCKER-dind
 sleep 3
 docker -H tcp://localhost:12375 version
 docker -H tcp://localhost:12375 swarm init
