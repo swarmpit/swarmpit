cca206de252d40787829ae9bad488967d011be1b
Lumir Mrkva <mrq@mrq.cz>
2019-12-09 22:24:49 +0100

    docker updated

--- dev/script/deploy-image.sh
+++ dev/script/deploy-image.sh
@@ -28,7 +28,7 @@ docker run --rm --privileged docker/binfmt:820fdd95a9972a5308930a2bdfb8573dd4447
 docker buildx create --name multi
 docker buildx use multi
 docker buildx inspect --bootstrap
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t $IMAGE --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t $IMAGE .
 
 if [ $TRAVIS_SECURE_ENV_VARS == "true" ] 
 then
