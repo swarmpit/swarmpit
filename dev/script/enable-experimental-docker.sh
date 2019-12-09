1dfd1d3a24754923b7131cc294bf28ee752c6f44
Lumir Mrkva <mrq@mrq.cz>
2019-12-09 22:12:32 +0100

    multiarch build on travis with buildx

---
+++ dev/script/enable-experimental-docker.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo '{"experimental":true}' | sudo tee /etc/docker/daemon.json
+mkdir $HOME/.docker
+touch $HOME/.docker/config.json
+echo '{"experimental":"enabled"}' | sudo tee $HOME/.docker/config.json
+sudo service docker restart
