5ce5ef4b480186407038ee7baf238592e5df1340
Lumir Mrkva <lumir.mrkva@topmonks.com>
2019-12-20 20:02:09 +0100

    fallback for missing list
    #441

--- src/clj/swarmpit/docker/registry/client.clj
+++ src/clj/swarmpit/docker/registry/client.clj
@@ -18,18 +18,25 @@
                 :options {:headers {:Authorization (str "Bearer " token)}}})
       :body))
 
+(defn- request-manifest
+  [token repository-name repository-tag type]
+  (let [response (execute {:method  :GET
+                           :api     (str "/" repository-name "/manifests/" repository-tag)
+                           :options {:headers {:Authorization (str "Bearer " token)
+                                               :Accept        type}}})
+        response-type (get-in response [:headers :content-type])]
+    (when (= type response-type) response)))
+
 (defn manifest
   [token repository-name repository-tag]
-  (-> (execute {:method  :GET
-                :api     (str "/" repository-name "/manifests/" repository-tag)
-                :options {:headers {:Authorization (str "Bearer " token)}}})
-      :body))
+  (:body (request-manifest token repository-name repository-tag
+                           "application/vnd.docker.distribution.manifest.v1+prettyjws")))
 
 (defn digest
   [token repository-name repository-tag]
-  (-> (execute {:method  :GET
-                :api     (str "/" repository-name "/manifests/" repository-tag)
-                :options {:headers {:Authorization (str "Bearer " token)
-                                    :Accept        "application/vnd.docker.distribution.manifest.list.v2+json"}}})
+  (-> (or (request-manifest token repository-name repository-tag
+                            "application/vnd.docker.distribution.manifest.list.v2+json")
+          (request-manifest token repository-name repository-tag
+                            "application/vnd.docker.distribution.manifest.v2+json"))
       :headers
       :docker-content-digest))
