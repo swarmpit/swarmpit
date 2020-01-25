8833047c54382a93a9d4df23fe55e55a88eeba60
Lumir Mrkva <mrq@mrq.cz>
2020-01-25 02:42:06 +0100

    reworked docker image from debian for newer jre and better arm support
    #425

---
+++ dev/script/install-docker-client.sh
@@ -0,0 +1,15 @@
+version=${DOCKER_VERSION:-"19.03.5"}
+case $(uname -m) in
+    armv6*)  arch=armhf ;;
+    armv7*)  arch=armhf ;;
+    armv8*)  arch=aarch64 ;;
+    arm64*)  arch=aarch64 ;;
+    aarch64) arch=aarch64 ;;
+    *)       arch=x86_64 ;;
+esac
+arch=${ARCH:-$arch}
+url=https://download.docker.com/linux/static/stable/"$arch"/docker-"$version".tgz
+echo installing $url
+cd /tmp || exit
+curl $url | tar xz docker/docker
+mv docker/docker /usr/bin/docker
