9380e10bcf31b2ed2e092813baf58a60f586ccdd
lumir-mrkva <lumir.mrkva@topmonks.com>
2020-05-20 19:37:11 +0200

    export

--- dev/script/start-dind.sh
+++ dev/script/start-dind.sh
@@ -2,7 +2,7 @@
 
 docker run --privileged --name dind -p 12375:2375 -e DOCKER_TLS_CERTDIR='' -d docker:$DOCKER-dind
 sleep 3
-DOCKER_API_VERSION=1.30
+export DOCKER_API_VERSION=1.30
 docker -H tcp://localhost:12375 version
 docker -H tcp://localhost:12375 swarm init
 docker -H tcp://localhost:12375 load -i $(dirname "${BASH_SOURCE[0]}")/hello-world.tar
