572fd052b0232a7226cc16371fc4a9a310ee96d0
Pavol Noha <pavol.noha@gmail.com>
2020-08-31 15:17:04 +0200

    github client #402

---
+++ src/clj/swarmpit/github/client.clj
@@ -0,0 +1,64 @@
+(ns swarmpit.github.client
+  (:require [swarmpit.http :refer :all]
+            [graphql-builder.parser :refer [defgraphql]]
+            [graphql-builder.core :as core]))
+
+(defgraphql graphql-queries
+            "graphql/github/user_packages.graphql"
+            "graphql/github/org_packages.graphql")
+
+(defn- graphql-response
+  [result]
+  (let [result-keys (set (keys result))]
+    (if (contains? result-keys :errors)
+      (let [error (first (:errors result))]
+        (throw
+          (ex-info (str "Github error: " (:message error))
+                   {:status 400
+                    :type   :http-client
+                    :body   {:error (:message error)}})))
+      (:data result))))
+
+(def query-map (core/query-map graphql-queries))
+
+(defn- execute
+  [{:keys [method url api options]}]
+  (execute-in-scope {:method        method
+                     :url           (str url api)
+                     :options       (merge {:insecure? true} options)
+                     :scope         "Github"
+                     :error-handler #(or (:error %) (:message %))}))
+
+(defn orgs
+  [registry]
+  (-> (execute {:method  :GET
+                :url     (:githubUrl registry)
+                :api     "/user/orgs"
+                :options {:headers {:Authorization (str "token " (:token registry))}}})
+      :body))
+
+(def org-packages-query (get-in query-map [:query :org-packages]))
+
+(defn org-packages
+  [registry org]
+  (let [query (org-packages-query {:login org})]
+    (-> (execute {:method  :POST
+                  :url     (:githubUrl registry)
+                  :api     "/graphql"
+                  :options {:headers {:Authorization (str "bearer " (:token registry))}
+                            :body    (:graphql query)}})
+        :body
+        (graphql-response))))
+
+(def user-packages-query (get-in query-map [:query :user-packages]))
+
+(defn user-packages
+  [registry]
+  (let [query (user-packages-query {:login (:username registry)})]
+    (-> (execute {:method  :POST
+                  :url     (:githubUrl registry)
+                  :api     "/graphql"
+                  :options {:headers {:Authorization (str "bearer " (:token registry))}
+                            :body    (:graphql query)}})
+        :body
+        (graphql-response))))
---
+++ src/clj/swarmpit/github/mapper.clj
@@ -0,0 +1,8 @@
+(ns swarmpit.github.mapper
+  (:require [clojure.string :as str]))
+
+(defn ->package-name [response type]
+  (let [nodes (get-in response [type :packages :nodes])]
+    (map #(str (get-in % [:repository :nameWithOwner])
+               "/"
+               (:name %)) nodes)))
