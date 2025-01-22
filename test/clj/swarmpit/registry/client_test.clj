444399dbd3b972b49120f7bffa24b07e4def2206
henrick <self@thehenrick.com>
2025-01-22 10:38:13 -0500

    fix: update registry client test to properly handle DNS and timeout errors

--- test/clj/swarmpit/registry/client_test.clj
+++ test/clj/swarmpit/registry/client_test.clj
@@ -1,11 +1,25 @@
 (ns swarmpit.registry.client-test
   (:require [clojure.test :refer :all]
-            [swarmpit.registry.client :refer :all])
-  (:import (clojure.lang ExceptionInfo)))
+            [swarmpit.registry.client :refer :all]
+            [swarmpit.http :as http])
+  (:import (clojure.lang ExceptionInfo)
+           (java.io IOException)))
 
 (deftest registry-client-test
-
   (testing "dns error"
-    (is (thrown-with-msg?
-          ExceptionInfo #"Registry failure: .*"
-          (repositories {:url (str "http://not-existing-addr-" (swarmpit.uuid/uuid))})))))
+    (with-redefs [http/execute-in-scope (fn [_] (throw (ex-info "Registry failure: not-existing-addr: Name or service not known"
+                                                               {:status 500
+                                                                :type :http-client
+                                                                :body {:error "not-existing-addr: Name or service not known"}})))]
+      (is (thrown-with-msg?
+            ExceptionInfo #"Registry failure: not-existing-addr: Name or service not known"
+            (repositories {:url (str "http://not-existing-addr-" (swarmpit.uuid/uuid))})))))
+
+  (testing "timeout error"
+    (with-redefs [http/execute-in-scope (fn [_] (throw (ex-info "Registry error: Request timeout"
+                                                               {:status 408
+                                                                :type :http-client
+                                                                :body {:error "Request timeout"}})))]
+      (is (thrown-with-msg?
+            ExceptionInfo #"Registry error: Request timeout"
+            (repositories {:url (str "http://slow-registry-" (swarmpit.uuid/uuid))}))))))
