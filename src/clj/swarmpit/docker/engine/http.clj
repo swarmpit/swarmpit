b28fcd938d1a4d661aa4ddbf7c2589755ca0a648
Jaben Cargman <Jaben@users.noreply.github.com>
2026-04-04 13:54:03 -0400

    Fix connection pool exhaustion from leaked timed-out connections
    When Docker is slow, with-timeout fires (5s) and calls future-cancel,
    which doesn't close the underlying socket. The socket stays open until
    Apache's SO_TIMEOUT (30s), leaking the pool connection for up to 25s.
    With 20 max connections and many concurrent subscriptions, the pool
    exhausts within seconds.
    
    - Align socket-timeout with request timeout so Apache kills timed-out
      sockets and properly returns connections to the pool
    - Make with-timeout a safety net (+5s buffer) so Apache always handles
      cleanup first
    - Increase pool from 20 to 50 for concurrent subscription headroom
    - Increase default timeout from 5s to 15s to tolerate slow Docker

--- src/clj/swarmpit/docker/engine/http.clj
+++ src/clj/swarmpit/docker/engine/http.clj
@@ -4,9 +4,9 @@
             [cheshire.core :refer [parse-string generate-string]]
             [swarmpit.config :refer [config]]
             [swarmpit.utils :refer [parse-int]])
-  (:import (org.apache.http.config RegistryBuilder)
+  (:import (org.apache.http.config RegistryBuilder SocketConfig)
            (swarmpit.socket UnixSocketFactory)
-           (org.apache.http.impl.conn BasicHttpClientConnectionManager)))
+           (org.apache.http.impl.conn PoolingHttpClientConnectionManager)))
 
 (defn- http?
   [] (not (nil? (re-matches #"^https?:\/\/.*" (config :docker-sock)))))
@@ -17,15 +17,30 @@
       (.register "http" (UnixSocketFactory/createUnixSocketFactory (config :docker-sock)))
       (.build)))
 
-(defn- make-unix-conn-manager
+(defn- make-pooling-unix-conn-manager
   []
-  (BasicHttpClientConnectionManager. (unix-scheme)))
+  (let [socket-config (-> (SocketConfig/custom)
+                          (.setSoTimeout 30000)
+                          (.build))]
+    (doto (PoolingHttpClientConnectionManager. (unix-scheme))
+      (.setMaxTotal 50)
+      (.setDefaultMaxPerRoute 50)
+      (.setValidateAfterInactivity 1000)
+      (.setDefaultSocketConfig socket-config))))
 
-(defn- make-conn-manager
+(defn make-conn-manager
   []
   (if (http?)
-    (conn-mgr/make-regular-conn-manager {})
-    (make-unix-conn-manager)))
+    (conn-mgr/make-reusable-conn-manager {:timeout 10 :threads 50 :default-per-route 50})
+    (make-pooling-unix-conn-manager)))
+
+(defonce ^:private shared-conn-manager (delay (make-conn-manager)))
+
+(defn get-conn-manager
+  "Returns shared connection manager. Exists as a function so tests
+   can with-redefs to supply a fresh manager for different configs."
+  []
+  @shared-conn-manager)
 
 (defn- url
   [uri]
@@ -37,10 +52,14 @@
 
 (defn execute
   [{:keys [method api options]}]
-  (execute-in-scope {:method        method
-                     :url           (url api)
-                     :options       (merge {:connection-manager (make-conn-manager)
-                                            :retry-handler      (fn [& _] false)} options)
-                     :scope         "Docker"
-                     :timeout       (parse-int (config :docker-http-timeout))
-                     :error-handler :message}))
+  (let [timeout-ms (or (parse-int (config :docker-http-timeout)) 5000)]
+    (execute-in-scope {:method        method
+                       :url           (url api)
+                       :options       (merge {:connection-manager          (get-conn-manager)
+                                              :connection-request-timeout  timeout-ms
+                                              :socket-timeout              timeout-ms
+                                              :connection-timeout          (max timeout-ms 5000)
+                                              :retry-handler               (fn [& _] false)} options)
+                       :scope         "Docker"
+                       :timeout       (+ timeout-ms 5000)
+                       :error-handler :message})))
